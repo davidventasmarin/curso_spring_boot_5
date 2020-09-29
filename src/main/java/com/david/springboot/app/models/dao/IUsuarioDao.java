b15d59956b812f48df42906d0f986d6a33f4386b
davidventasmarin <dvm@medlinetec.eu>
2020-09-29 09:59:16 +0200

    Limpiado el paquete david

--- src/main/java/com/david/springboot/app/models/dao/IUsuarioDao.java
+++
@@ -1,8 +0,0 @@
-package com.david.springboot.app.models.dao;
-
-import com.david.springboot.app.models.entity.Usuario;
-import org.springframework.data.repository.CrudRepository;
-
-public interface IUsuarioDao extends CrudRepository<Usuario, Long> {
-    public Usuario findByUsername(String username);
-}
